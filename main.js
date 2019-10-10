05a719fb01338d911389d7d3c0034d4f46bc1360
Karlis Caune <karlis@qminderapp.com>
2019-10-10 19:33:26 +0300

    Fixes for IE11

--- main.js
+++ main.js
@@ -5,6 +5,62 @@ var placeNames = require('./js/names');
 var panellumTranslations = require('./js/translations');
 var OrbitControls = require('./js/orbitcontrols');
 var panellum = require('pannellum');
+var polyfill = require('url-polyfill');
+
+// Polyfills
+
+function objectValues(obj) {
+    return Object.keys(obj).map(function(k) {
+       return obj[k];
+    });
+};
+
+// https://tc39.github.io/ecma262/#sec-array.prototype.findindex
+if (!Array.prototype.findIndex) {
+    Object.defineProperty(Array.prototype, 'findIndex', {
+      value: function(predicate) {
+       // 1. Let O be ? ToObject(this value).
+        if (this == null) {
+          throw new TypeError('"this" is null or not defined');
+        }
+  
+        var o = Object(this);
+  
+        // 2. Let len be ? ToLength(? Get(O, "length")).
+        var len = o.length >>> 0;
+  
+        // 3. If IsCallable(predicate) is false, throw a TypeError exception.
+        if (typeof predicate !== 'function') {
+          throw new TypeError('predicate must be a function');
+        }
+  
+        // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
+        var thisArg = arguments[1];
+  
+        // 5. Let k be 0.
+        var k = 0;
+  
+        // 6. Repeat, while k < len
+        while (k < len) {
+          // a. Let Pk be ! ToString(k).
+          // b. Let kValue be ? Get(O, Pk).
+          // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
+          // d. If testResult is true, return k.
+          var kValue = o[k];
+          if (predicate.call(thisArg, kValue, k, o)) {
+            return k;
+          }
+          // e. Increase k by 1.
+          k++;
+        }
+  
+        // 7. Return -1.
+        return -1;
+      },
+      configurable: true,
+      writable: true
+    });
+  }
 
 // Get params
 
@@ -87,11 +143,11 @@ let panoramaViewer = null;
 
 // Place names
 
-const places = Object.values(placeNames.namePlanes);
+const places = objectValues(placeNames.namePlanes);
 
 // marker positions
 
-const markers = Object.values(markerOptions.options);
+const markers = objectValues(markerOptions.options);
 const panoramaCount = markers.length -1;
 var baseMeshes = [];
 var placeMeshes = [];
@@ -100,7 +156,7 @@ var panellumStrings = panellumTranslations.strings;
 
 // panorama order
 
-const pOrder = Object.values(panoramaOrder.order);
+const pOrder = objectValues(panoramaOrder.order);
 
 // Event listeners
 
