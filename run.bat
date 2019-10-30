f78fee44410213e8c23906702cd68a0568384d3d
Karlis Caune <karlis@grandem.lv>
2019-10-30 20:50:15 +0200

    updated bat file

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
-start c:\website\tiny.exe c:\website 8000
-start chrome.exe --kiosk http://localhost:8000/
+start c:\panoramas\tiny.exe c:\panoramas 8000
+start chrome.exe --kiosk http://localhost:8000/?autoplay=1^&wait=300^&speed=3
